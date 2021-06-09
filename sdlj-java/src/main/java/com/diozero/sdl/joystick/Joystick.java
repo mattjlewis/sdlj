4afbccab2b513c6729e0c2be3a6ddb43801cf27a
Matthew Lewis <matthew.lewis@mulesoft.com>
2021-06-09 14:10:35 +0100

    Updated tinylog

--- sdlj-java/src/main/java/com/diozero/sdl/joystick/Joystick.java
+++ sdlj-java/src/main/java/com/diozero/sdl/joystick/Joystick.java
@@ -1,6 +1,6 @@
 package com.diozero.sdl.joystick;
 
-import org.pmw.tinylog.Logger;
+import org.tinylog.Logger;
 
 public class Joystick extends JoystickInfo {
 	private int numAxes;
@@ -9,7 +9,7 @@ public class Joystick extends JoystickInfo {
 	private int numHats;
 	private JoystickEventListener listener;
 	private int eventTypeMask;
-	
+
 	Joystick(int id, String name, int numAxes, int numBalls, int numButtons, int numHats) {
 		super(id, name);
 		this.numAxes = numAxes;
@@ -33,32 +33,32 @@ public class Joystick extends JoystickInfo {
 	public int getNumHats() {
 		return numHats;
 	}
-	
+
 	public int getAxisValue(int axis) {
 		return JoystickNative.getAxisValue(getId(), axis);
 	}
-	
+
 	public boolean getButtonValue(int button) {
 		return JoystickNative.getButtonValue(getId(), button) == 1;
 	}
-	
-	public void setListener(JoystickEventListener listener, JoystickEvent.Type ... types) {
+
+	public void setListener(JoystickEventListener listener, JoystickEvent.Type... types) {
 		this.listener = listener;
 		setEnabledEventTypes(types);
 	}
-	
-	public void setEnabledEventTypes(JoystickEvent.Type ... types) {
+
+	public void setEnabledEventTypes(JoystickEvent.Type... types) {
 		eventTypeMask = 0;
 		for (JoystickEvent.Type type : types) {
 			eventTypeMask |= type.getMask();
 		}
 		Logger.debug("eventTypeMask={}", Integer.valueOf(eventTypeMask));
 	}
-	
+
 	public void removeListener() {
 		listener = null;
 	}
-	
+
 	void newEvent(JoystickEvent event) {
 		Logger.debug("eventTypeMask={}, mask hit={}", Integer.valueOf(eventTypeMask),
 				Integer.valueOf(eventTypeMask & event.getType().getMask()));
@@ -66,7 +66,7 @@ public class Joystick extends JoystickInfo {
 			listener.accept(event);
 		}
 	}
-	
+
 	public void close() {
 		JoystickNative.closeJoystick(this);
 	}
--- sdlj-java/src/main/java/com/diozero/sdl/joystick/JoystickNative.java
+++ sdlj-java/src/main/java/com/diozero/sdl/joystick/JoystickNative.java
@@ -1,64 +1,82 @@
 package com.diozero.sdl.joystick;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-import org.pmw.tinylog.Logger;
+import org.tinylog.Logger;
 
 public class JoystickNative {
 	private static final String LIB_NAME = "sdljoystick";
 	private static Boolean loaded = Boolean.FALSE;
 	private static Map<Integer, Joystick> joysticks;
-	
-	private static void init() {
-		synchronized (loaded) {
-			if (!loaded.booleanValue()) {
-				try {
+
+	private synchronized static void init() {
+		if (loaded == Boolean.FALSE) {
+			try (InputStream is = Thread.currentThread().getContextClassLoader()
+					.getResourceAsStream("/lib/lib" + LIB_NAME + ".so")) {
+				if (is != null) {
 					Path path = Files.createTempFile("lib" + LIB_NAME, ".so");
-					path.toFile().deleteOnExit();
-					Files.copy(Joystick.class.getResourceAsStream("/lib/lib" + LIB_NAME + ".so"),
-							path, StandardCopyOption.REPLACE_EXISTING);
-					System.load(path.toString());
-				} catch (IOException e) {
-					Logger.error(e, "Error loading library from classpath: {}", e);
-					
-					// Try load the usual way...
-					System.loadLibrary(LIB_NAME);
+					Files.copy(is, path, StandardCopyOption.REPLACE_EXISTING);
+					Runtime.getRuntime().load(path.toString());
+					loaded = Boolean.TRUE;
+					path.toFile().delete();
 				}
-				
-				if (initialise() < 0) {
-					throw new RuntimeException("Error initialising SDL Joystick library");
-				}
-				
-				loaded = Boolean.TRUE;
-				
-				joysticks = new HashMap<>();
-				
-				Runtime.getRuntime().addShutdownHook(new Thread(() -> { Logger.info("Terminating"); terminate(); }));
+			} catch (IOException e) {
+				Logger.info(e, "Error loading library from classpath: {}", e);
 			}
+
+			if (loaded == Boolean.FALSE) {
+				// Try load the usual way...
+				System.loadLibrary(LIB_NAME);
+				loaded = Boolean.TRUE;
+			}
+
+			if (initialise() < 0) {
+				throw new RuntimeException("Error initialising SDL Joystick library");
+			}
+
+			joysticks = new HashMap<>();
+
+			Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+				Logger.info("Terminating");
+				terminate();
+			}));
 		}
 	}
-	
+
 	static {
 		init();
 	}
-	
+
 	private static native int initialise();
+
 	private static native void terminate();
+
 	private static native Joystick openJoystick(int id);
+
 	private static native void closeJoystick(int id);
+
 	public static native JoystickEvent waitForEvent();
+
 	private static native String[] listJoysticksNative();
-	
+
 	public static native int getNumJoysticks();
+
 	public static native int getAxisValue(int id, int axis);
+
 	public static native Ball getBallValue(int id, int ball);
+
 	public static native int getButtonValue(int id, int button);
+
 	public static native int getHatValue(int id, int hat);
-	
+
 	public static Joystick getJoystick(int id) {
 		Joystick joystick;
 		synchronized (joysticks) {
@@ -66,27 +84,27 @@ public class JoystickNative {
 			if (joystick != null) {
 				return joystick;
 			}
-			
+
 			Logger.debug("Calling openJoystick({})", Integer.valueOf(id));
 			joystick = openJoystick(id);
 			Logger.debug("After openJoystick({})", Integer.valueOf(id));
 			if (joystick == null) {
 				return null;
 			}
-			
+
 			Logger.debug("Adding joystick {} to hashmap", Integer.valueOf(id));
 			joysticks.put(Integer.valueOf(id), joystick);
 		}
-		
+
 		return joystick;
 	}
-	
+
 	static void closeJoystick(Joystick joystick) {
 		Logger.debug("Removing joystick {} from hashmap", Integer.valueOf(joystick.getId()));
 		joysticks.remove(Integer.valueOf(joystick.getId()));
 		closeJoystick(joystick.getId());
 	}
-	
+
 	public static void processEvents() {
 		while (true) {
 			JoystickEvent event = waitForEvent();
@@ -102,18 +120,18 @@ public class JoystickNative {
 			}
 		}
 	}
-	
+
 	public static List<JoystickInfo> listJoysticks() {
 		String[] j_names = listJoysticksNative();
 		List<JoystickInfo> j_infos = new ArrayList<>();
 		if (j_names == null) {
 			return j_infos;
 		}
-		
-		for (int i=0; i<j_names.length; i++) {
+
+		for (int i = 0; i < j_names.length; i++) {
 			j_infos.add(new JoystickInfo(i, j_names[i]));
 		}
-		
+
 		return j_infos;
 	}
 }
